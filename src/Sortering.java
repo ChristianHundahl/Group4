f3371a8478bd2422f8c37140e4924b751467f209
ChristianHundahl <chri02jx@stud.kea.dk>
2021-04-06 15:39:21 +0200

    2. push Sortering

--- src/Sortering.java
+++ src/Sortering.java
@@ -1,22 +1,21 @@
 import  java.util.ArrayList;
+import java.util.Scanner;
 
 public class Sortering {
     String problemDescription = "Skriv en metode der indlæser 5 tekststrenge fra tastaturet og udskriver dem på skærmen i alfabetiskfaldende orden på skærmen";
 
     public static void Sortering() {
-        ArrayList<String> arrayList = new ArrayList<>(); //Create ArrayList
-        arrayList.add("Abekat"); //Add spcified items to ArrayList
-        arrayList.add("Musefælde");
-        arrayList.add("Ananas");
-        arrayList.add("Slut");
-        arrayList.add("Solskin");
+        ArrayList<String> sortThisList = new ArrayList<>(); //Create ArrayList
+        //TODO: add input from scanner to sortThisList
+        Scanner userInput = new Scanner();
 
-        for (int i = 0; i < arrayList.size(); i++) //Create loop to print contents in regular order
-            System.out.println(arrayList.get(i));
+        //TODO: Create 'for' loop to put items in alphabetical order
+        for (int i = 0; i < sortThisList.size(); i++) //Create loop to print sortThisList in original order
+            System.out.println(sortThisList.get(i)); //Print sortThisList in original order
 
         System.out.println(); //Linebreak
 
-        for (int i = arrayList.size() -1; i >= 0; i--) //Create loop to print contents in reverse order
-            System.out.println(arrayList.get(i));
+        for (int i = sortThisList.size() -1; i >= 0; i--) //Create loop to print sortThisList in reverse order
+            System.out.println(sortThisList.get(i)); //Print sortThisList in reverse order
     }
 }
