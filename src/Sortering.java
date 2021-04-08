2b11f3a69021412933ad0963028807d378472d3d
ChristianHundahl <chri02jx@stud.kea.dk>
2021-04-08 11:42:45 +0200

    6. push Sortering --- tilføjede metode til at printe problemStatement

--- src/Sortering.java
+++ src/Sortering.java
@@ -3,22 +3,22 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 public class Sortering {
-    public static void SorteringsMetode() {
-        System.out.println();
-        String problemStatement = "Skriv en metode der indlæser 5 tekststrenge fra tastaturet og udskriver dem på skærmen i alfabetiskfaldende orden på skærmen";
+    private String problemStatement;
+    //Skriv en metode der indlæser 5 tekststrenge fra tastaturet og udskriver dem på skærmen i alfabetiskfaldende orden på skærmen
+    public void printProblemDescription(String problemStatement){
+        this.problemStatement = problemStatement;
         System.out.println(problemStatement);
+    }
+
+    public static void SorteringsMetode() {
         ArrayList<String> sortThisList = new ArrayList<>(); //Create ArrayList
-        //TODO: add input from scanner to sortThisList
         Scanner userInput = new Scanner(System.in);
         System.out.println("Skriv fem tekststrenge her:");
 
-        //TODO: Create loop to get 5 Strings from user
-        for (int i = 0; i < 5; i++) {
-             sortThisList.add(userInput.nextLine());
+        for (int i = 0; i < 5; i++) { //Ask for 5 required inputs
+             sortThisList.add(userInput.nextLine()); //Get input from user
         }
         System.out.println("Listen før den kommer i bagvendt alfabetisk orden: " + sortThisList);
-
-        //TODO: Create 'for' loop to put items in alphabetical order
         sortThisList.sort(Comparator.reverseOrder());
         System.out.println("Listen efter den kommer i bagvendt alfabetisk orden: " + sortThisList);
         System.out.println();
