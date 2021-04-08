02f790f817eb256b1312bf63487e739d723a3a33
ChristianHundahl <chri02jx@stud.kea.dk>
2021-04-08 11:03:33 +0200

    5. push Sortering --- oversatte print statements til dansk --- tilføjede linjeskift før og efter metoden

--- src/Sortering.java
+++ src/Sortering.java
@@ -1,24 +1,26 @@
-import  java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Scanner;
 
 public class Sortering {
-    //Skriv en metode der indlæser 5 tekststrenge fra tastaturet og udskriver dem på skærmen i alfabetiskfaldende orden på skærmen
-
     public static void SorteringsMetode() {
+        System.out.println();
+        String problemStatement = "Skriv en metode der indlæser 5 tekststrenge fra tastaturet og udskriver dem på skærmen i alfabetiskfaldende orden på skærmen";
+        System.out.println(problemStatement);
         ArrayList<String> sortThisList = new ArrayList<>(); //Create ArrayList
         //TODO: add input from scanner to sortThisList
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Input 5 Strings to add to the list:");
+        System.out.println("Skriv fem tekststrenge her:");
 
         //TODO: Create loop to get 5 Strings from user
         for (int i = 0; i < 5; i++) {
              sortThisList.add(userInput.nextLine());
         }
-        System.out.println("List before sorting in reverse alphabetical order: " + sortThisList);
+        System.out.println("Listen før den kommer i bagvendt alfabetisk orden: " + sortThisList);
 
         //TODO: Create 'for' loop to put items in alphabetical order
         sortThisList.sort(Comparator.reverseOrder());
-        System.out.println("List after sorting in reverse alphabetical order: " + sortThisList);
+        System.out.println("Listen efter den kommer i bagvendt alfabetisk orden: " + sortThisList);
+        System.out.println();
     }
 }
